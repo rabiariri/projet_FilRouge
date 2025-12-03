f31a3ae4116ac67acae0d44e7e7346ecee7ec211
Sankilicoglur <rabiasankilicoglu@gmail.com>
2025-12-03 16:47:05 +0100

    Premier push

---
+++ Exercice1/script.js
@@ -0,0 +1,18 @@
+// - Créer une variable pour le nom de la boutique
+let shopName = "JeSuis";
+// - Créer une variable pour la ville
+let city = "Lyon";
+// - Créer une variable booléenne pour savoir si la boutique est ouverte
+let isOpen = true;
+// - Créer une variable pour le nombre de produits
+let salesCount = 0;
+// - Créer une variable pour le slogan
+let slogan = "une iencli";
+// - Afficher un message de bienvenue dans la console
+let welcomeMessage = "bienvenue";
+// - Afficher le slogan dans la console'
+console.log(slogan);
+
+//   - afficher dans la console un message “ouverte”
+console.log(isOpen);
+//   - ou un message “fermée”
